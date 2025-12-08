6382363435646740702aa478c826c20ebdbf556e
BYNNΛI <233502243+BYNNAI@users.noreply.github.com>
2025-12-08 22:58:07 +0000

    Delete src/automatonsec directory

--- src/automatonsec/config/__init__.py
+++
@@ -1,5 +0,0 @@
-# BYNNΛI - AutomatonSec (https://github.com/BYNNAI/AutomatonSec)
-
-from automatonsec.config.config_loader import AnalysisConfig, load_config
-
-__all__ = ["AnalysisConfig", "load_config"]
--- src/automatonsec/core/__init__.py
+++
@@ -1,8 +0,0 @@
-# BYNNΛI - AutomatonSec (https://github.com/BYNNAI/AutomatonSec)
-
-"""Core analysis engine components"""
-
-from automatonsec.core.engine import SecurityEngine
-from automatonsec.core.models import AnalysisResult, Vulnerability
-
-__all__ = ["SecurityEngine", "AnalysisResult", "Vulnerability"]
